decba3dd011c19ef9373aa40e999babde4a993c4
shimishimizu <h200l700@gmail.com>
2024-06-07 00:30:07 +0900

    types.ts追加

---
+++ _types/types.ts
@@ -0,0 +1,13 @@
+export type PostType = {
+  id: string;
+  userId: string;
+  title: string;
+  body: string;
+};
+
+// export type PostsType = {
+//   id: string;
+//   userId: string;
+//   title: string;
+//   body: string;
+// }[];
